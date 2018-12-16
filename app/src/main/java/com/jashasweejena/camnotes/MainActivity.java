71e66dfb040406953a84a3a4197bea553ca81324
Jashaswee Jena <j.ishujena2000@gmail.com>
2018-12-16 14:49:33 +0530

    second

--- app/src/main/java/com/jashasweejena/camnotes/MainActivity.java
+++ app/src/main/java/com/jashasweejena/camnotes/MainActivity.java
@@ -3,7 +3,6 @@ package com.jashasweejena.camnotes;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -29,29 +28,29 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.asksira.bsimagepicker.BSImagePicker;
 import com.fxn.pix.Pix;
 import com.fxn.utility.PermUtil;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements BSImagePicker.OnSingleImageSelectedListener, BSImagePicker.OnMultiImageSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     static final int REQUEST_TAKE_PHOTO = 1;
     static final int PIX_REQUEST_CODE = 2;
     static final int MAX_SELECTION_COUNT = 5;
+    static final int SCAN_REQUEST_CODE = 99;
     Context context = MainActivity.this;
     final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
     final String orderBy = MediaStore.Images.Media._ID;
@@ -69,17 +68,17 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getFromSdcard();
         imgPickerButton = findViewById(R.id.imgPickerBtn);
-        imagegrid = (GridView) findViewById(R.id.PhoneImageGrid);
+        imagegrid = findViewById(R.id.PhoneImageGrid);
         imageAdapter = new ImageAdapter();
         imagegrid.setAdapter(imageAdapter);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -154,32 +153,16 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
         }
     }
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
     public void getFromSdcard() {
         File file = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         f.clear();
-        if (file.isDirectory()) {
+        if (file != null && file.isDirectory()) {
             listOfFiles = file.listFiles();
 
 
-            for (int i = 0; i < listOfFiles.length; i++) {
+            for (File listOfFile : listOfFiles) {
 
-                f.add(listOfFiles[i].getAbsolutePath());
+                f.add(listOfFile.getAbsolutePath());
 
             }
         }
@@ -187,41 +170,84 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) { //When camera returns photo to calling activity
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            getFromSdcard();
-            imagegrid.setAdapter(imageAdapter);
-        }
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            if (requestCode == SCAN_REQUEST_CODE) {
+                Uri uri = Objects.requireNonNull(data.getExtras()).getParcelable(ScanConstants.SCANNED_RESULT);
+                Bitmap bitmap;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    if (uri != null) {
+                        getContentResolver().delete(uri, null, null);
+                    }
+                    String path = storeImage(bitmap);
+                    f.add(path);
+                    imageAdapter.notifyDataSetChanged();
+                    imagegrid.setAdapter(imageAdapter);
 
-        if (resultCode == Activity.RESULT_OK && requestCode == PIX_REQUEST_CODE) {
-            ArrayList<String> returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS);
-
-            for (String x : returnValue) {
-                f.add(x);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
-            imageAdapter.notifyDataSetChanged();
-            imagegrid.setAdapter(imageAdapter);
-
-
         }
     }
 
-    @Override
-    public void onSingleImageSelected(Uri uri, String tag) {
-        Log.d(TAG, "onSingleImageSelected: " + uri);
+    private String storeImage(Bitmap image) {
+        File pictureFile = null;
+        try {
+            pictureFile = createImageFile();
+            if (pictureFile == null) {
+                Log.d(TAG,
+                        "Error creating media file, check storage permissions: ");// e.getMessage());
+                return null;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pictureFile != null ? pictureFile.getAbsolutePath() : null;
     }
 
-    @Override
-    public void onMultiImageSelected(List<Uri> uriList, String tag) {
-        Log.d(TAG, "onMultiImageSelected: " + uriList.get(uriList.size() - 1));
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Files");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName="MI_"+ timeStamp +".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
     }
 
     void imagePicker() {
 
-        Pix.start(MainActivity.this,                    //Activity or Fragment Instance
-                PIX_REQUEST_CODE,                //Request code for activity results
-                MAX_SELECTION_COUNT);    //Number of images to restict selection count
+//        Pix.start(MainActivity.this,                    //Activity or Fragment Instance
+//                PIX_REQUEST_CODE,                //Request code for activity results
+//                MAX_SELECTION_COUNT);    //Number of images to restict selection count
 
+        Intent intent = new Intent(this, ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, ScanConstants.OPEN_CAMERA);
+        startActivityForResult(intent, SCAN_REQUEST_CODE);
 
     }
 
@@ -235,7 +261,6 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
                 } else {
                     Toast.makeText(MainActivity.this, "Approve permissions to open Pix ImagePicker", Toast.LENGTH_LONG).show();
                 }
-                return;
             }
         }
     }
@@ -247,7 +272,7 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             String imageFileName = "PDF_" + timeStamp + "_";
 
-            String dest = directoryPath + "/CamNotes/" + imageFileName + ".pdf"; //  Change pdf's name.
+            String dest = directoryPath + "/" + imageFileName + ".pdf"; //  Change pdf's name.
 
             Image img = Image.getInstance(f.get(0));
             Document document = new Document(img);
@@ -262,6 +287,7 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
             }
             document.close();
         } catch (Exception e) {
+            Log.d("devil: ",e.getMessage());
             e.printStackTrace();
         }
     }
@@ -270,7 +296,7 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
 
         for (String x : f) {
             Uri uri = Uri.parse(x);
-            File file = new File(uri.getPath());
+            File file = new File(Objects.requireNonNull(uri.getPath()));
             if (file.exists()) {
                 if (file.delete()) {
                     Log.d(TAG, "clearGridView: " + "File deleted");
@@ -286,7 +312,7 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
     public class ImageAdapter extends BaseAdapter {
         private LayoutInflater mInflater;
 
-        public ImageAdapter() {
+        ImageAdapter() {
             mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         }
@@ -349,31 +375,6 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnS
         CheckBox checkBox;
         int id;
 
-    }
-
-    public static void saveListToSharedPref(Context context, ArrayList<String> parentList) {
-        SharedPreferences mPrefs = context.getSharedPreferences("f", context.MODE_PRIVATE);
-        SharedPreferences.Editor prefsEditor = mPrefs.edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(parentList);
-        prefsEditor.putString("myJson", json);
-        prefsEditor.commit();
-    }
-
-    public static ArrayList<String> getListFromSharedPref(Context context) {
-        ArrayList<String> savedCollage = new ArrayList<String>();
-        SharedPreferences mPrefs = context.getSharedPreferences("f", context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = mPrefs.getString("myJson", "");
-        if (json.isEmpty()) {
-            savedCollage = new ArrayList<String>();
-        } else {
-            Type type = new TypeToken<List<String>>() {
-            }.getType();
-            savedCollage = gson.fromJson(json, type);
-        }
-
-        return savedCollage;
     }
 
 }
